1384b1710dd4f9e0be667c0fce605778e4f519fe
kelvinchinedu1220 <pilopilosimon1990@gmail.com>
2025-10-19 05:21:21 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -29,7 +29,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
     export OF_NO_RELOAD_AFTER_DECRYPTION=1
     export FOX_DISABLE_APP_MANAGER=1
     export OF_MAINTAINER=nino
-    export FOX_VERSION=R14.1
+	export FOX_MAINTAINER_PATCH_VERSION=R14.1
     export FOX_BUILD_TYPE="Beta"
     export FOX_VARIANT=A14
     export OF_SCREEN_H=2400
